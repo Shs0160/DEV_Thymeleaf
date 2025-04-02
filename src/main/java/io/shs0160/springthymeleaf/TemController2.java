9ddefb6cdbf212624faefefc154f1eff3a5e8cff
Shs0160 <aladinsue@gmail.com>
2025-04-02 14:13:52 +0900

    문법 8

--- src/main/java/io/shs0160/springthymeleaf/TemController2.java
+++ src/main/java/io/shs0160/springthymeleaf/TemController2.java
@@ -68,10 +68,17 @@ public class TemController2 {
     }
 
     @GetMapping("/7")
-    public String syntaxPage7() {
+    public String syntaxPage7(Model model) {
+        model.addAttribute("username", "user1");
         return "/syntax/page7";
     }
 
+    @GetMapping("/8")
+    public String syntaxPage8(Model model) {
+        model.addAttribute("blue", "blue");
+        model.addAttribute("red", "red");
+        return "/syntax/page8";
+    }
 
 
 
